0bbf300b258aaab7c5abbbdb44203d7b70d46fcb
Moussa Elrahmoun <111162097+h672637@users.noreply.github.com>
2024-01-24 13:00:31 +0100

    test
    ny

--- databaseEksempel/src/main/java/no/hvl/dat108/webshop/Rute.java
+++ databaseEksempel/src/main/java/no/hvl/dat108/webshop/Rute.java
@@ -4,6 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Rute {
-	
-	
+    private int rute;
+    private int ersleng=0;
+    private int stige=0;
+
+    public Rute(int rute ){
+     this.rute=rute;
+    }
+    public int  GetRute(){
+        return rute ;
+    }
+    public void SetNyRute(int nyrute ){
+        this.rute=nyrute;
+    }
+    public void setTilSlenge(int pos){
+        if()
+        this.ersleng=pos;
+    }
+    
 }
